46de5145af2ff715a9287ac4fc6a510f0c865e1a
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-07-09 00:15:55 +0530

    Add all functionalities of event management website

---
+++ block-BNaaft/altEvents/models/remark.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var remarkSchema = new Schema({
+    title: { type: String, required: true },
+    author: String,
+    likes: { type: Number, default: 0 },
+    eventId: { type: Schema.Types.ObjectId, ref: 'Event', required: true }
+}, { timestamps: true });
+
+var Remark = mongoose.model('Remark', remarkSchema);
+
+module.exports = Remark;
